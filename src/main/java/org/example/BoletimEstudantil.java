3624eaca119ba807ee9e2799d89bd9a71decadd8
Marcelo Santos <rocha.mer21@gmail.com>
2023-03-31 21:10:39 -0300

    identacao

--- src/main/java/org/example/BoletimEstudantil.java
+++ src/main/java/org/example/BoletimEstudantil.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import java.util.Random;
+
 public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = new Random().nextInt(10);
+
+        if (mediaFinal < 6) System.out.println("REPROVADO");
+        else if (mediaFinal == 6) System.out.println("PROVA MINERVA");
+        else System.out.println("APROVADO");
+    }
 }
